9cf16fd594ff3741e10054e6a03c54bcc805ebe5
E S Hoogland <hoogle.e@gmail.com>
2025-07-12 02:57:52 -0400

    registration.js etc

--- js/try-catch-throw-finally-error-handling.js
+++ js/try-catch-throw-finally-error-handling.js
@@ -29,8 +29,8 @@ try {
   console.log("start of try runs"); // "start of try runs" 
   nucamp; // error, there's no such variable!
   console.log("end of try (never reached)"); // skipped due to error
-} catch(err) {
-  console.log("Error has occurred", err.stack); // "Error has occurred ReferenceError: nucamp is not defined"
+} catch(error) {
+  console.log("Error has occurred", error.stack); // "Error has occurred ReferenceError: nucamp is not defined"
 } finally {
   console.log("this is the finally block"); // always runs
 }
