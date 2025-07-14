12bae7e9b01a1276ad6899b488c9120f6c2a0b5d
Eric Hoogland <hoogle.e@gmail.com>
2025-07-14 00:00:05 -0400

    submitted version

--- js/library.js
+++ js/library.js
@@ -1,5 +1,5 @@
 
-// I define a class called Books with a constructor method having three parameters.
+// class Book with a constructor method having three parameters.
 class Book {
     // the boolean parameter called available has a default value of true. // the constructor's three parameters are set 
         // as properties of the object 
@@ -12,57 +12,79 @@ class Book {
 } // end Books
 
         
-// This object contains an array of books--books-- 
-// and add, check out, and available methods to manage the books array.
+// This object contains an array of books and add, check out, and 
+// available methods to manage the books array.
 
 // This creates a new instance of an object using a constructor 
-      // using the new keyword and passing the appropriate arguments.
-  // An addBook method to add a new book to the library when a patron donates a book.
+// using the new keyword and passing the appropriate arguments.
+// An addBook method to add a new book to the library when a patron 
+// donates a book.
 
 const library = {
     books: [],
     addBook: function (title, author) {
         const book = new Book(title, author);
         this.books.push(book);
-        console.log(`You have added "${book.title}", by ${book.author} to our listings. 
-            Books in the library's database: ${this.books.length}`);
-            return;
-        }
-    }
-  
-        /*addBook("Moby-lsDick", "Herman Melville");
-        addBook(`How Not To Die`, `Michael Greger, M.D.`);
-        addBook("How Not To Diet", "Michael Greger, M.D.");        
-    
-    checkOutBook: function(title) {
-        for (let book of this.books) {
-            if (book.title === title && book.available) {
-                found = true;
-                book.available = false;
-            }
-            console.log(`Checked out: ${book.title}`)
-            return;
-        }
-    },
-    getAvailableBooks: function() {
-        let bookList = [];
-        for (let book of this.books) {
-            if (book.available) {
-                bookList.push(book.title);
-            }
-        }
-        console.log(`There are ${bookList.length} titles in the library's listings:              ${bookList.join(" -- ")}`) 
-        return;
-    },
-
-    
-} 
-
-addBook('How Not To Age', 'Michael Greger, M.D.');
-        addBook("Moby-lsDick", "Herman Melville");
-        addBook("How Not To Die", "Michael Greger, M.D.");
-        addBook("How Not To Diet", "Michael Greger, M.D.");
-
+        console.log(`\nAnother best-in-class book added:\n"${book.title}", by ${book.author}.`)
+        console.log(`Books in the library's database: ${this.books.length}`);
+        },  
         
-        checkOutBook("How Not To Diet");
-        getAvailableBooks(); */     
+        checkOutBook: function(title) {
+            for (let book of this.books) {
+                if (book.title === title && !book.available) {
+                    console.log(`\n${book.title} is already checked out`);
+                    return 
+                }
+                if (book.title === title && book.available) {
+                    found = true;
+                    book.available = false;
+                    console.log(`\nChecked out: ${book.title}`)
+                    return;
+                }
+                else {   
+                    console.log(`${title} is not in our collection.`)
+                    return;
+                }
+            }
+        },
+    
+        getAvailableBooks: function() {
+            let bookList = [];
+            for (let book of this.books) {
+                if (book.available) {
+                    bookList.push(book.title);
+                    console.log(`\nWe have ${bookList.length} available titles:\n\n${bookList.join("\n")}`);
+                }
+            return;
+        }   
+    }
+} // end library 
+const newBooks = `[
+        {"title": "How Not To Die", "author": "Michael Greger"},
+        {"title": "How Not To Diet", "author": "Michael Greger"},
+        {"title": "How Not To Age", "author": "Michael Greger"},
+        {"title": "Structure and Interpretation of Computer Programs", "author": "Gerald Sussmann"},
+        {"title": "Structure and Interpretation of Computer Programs, JavaScript edition", "author": "Gerald Sussmann"},
+        {"title": "The Little Schemer", "author": "Daniel P. Friedman"},
+        {"title": "JavaScript: The Good Parts", "author": "Douglas Crockford"},
+        {"title": "Eloquent JavaScript", "author": "Marijn Haverbeke"}
+        ]`;
+
+function receiveBooks(bookJSON) {
+    const booksToAdd = JSON.parse(bookJSON);
+    for (let book of booksToAdd) {
+        library.addBook(book.title, book.author);
+    }
+}
+
+
+// Tests
+console.log(`There are currently ${library.books.length} books in the library's database.`);
+library.addBook("Eloquent JavaScript", "Marijn Haverbeke");
+receiveBooks(newBooks);
+library.checkOutBook("Eloquent JavaScript");
+library.checkOutBook("Grokking the Coding Interview");
+library.getAvailableBooks();
+
+
+
